1488e932a300dde97974f99dd805e2e4e4e82fa8
RookieZB <chenzb_best@126.com>
2020-03-08 21:23:43 +0800

    Have added ALBERT.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Description
-Just my simple implementation of BERT by TensorFlow 2, mainly for the purpose of practicing TF. This code is partly incomplete (currently only support fine tuning on downstream tasks in Chinese using original BERT), and may have some mistakes. Perhaps later I will improve it.
+Just my simple implementation of BERT and ALBERT by TensorFlow 2, mainly for the purpose of practicing TF. This code is partly incomplete, and may have some mistakes.
 ## References
 https://tensorflow.google.cn/tutorials  
 https://github.com/google-research/bert  
+https://github.com/google-research/ALBERT  
+https://github.com/brightmart/albert_zh  
 https://github.com/bojone/bert4keras
--- bert_by_tf2.py
+++ bert_by_tf2.py
@@ -2,9 +2,10 @@
 
 """
 Title: BERT by TF2
-Version: 0.1.0
+Version: 0.1.1
 Author: Chris Chen
 Date: 2020/02/02
+Update: 2020/02/28
 
 """
 
@@ -38,14 +39,14 @@ def gelu_activating(x):
 
 class Attention(keras.layers.Layer):
     """The attention layer"""
-    def __init__(self, lname, head, size, drop, **kwargs):
+    def __init__(self, bname, lname, head, size, drop, **kwargs):
         super(Attention, self).__init__(**kwargs)
         self.head, self.size, self.ninf = head, size, -1e4
-        self.wq = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=lname+'/self/query')
-        self.wk = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=lname+'/self/key')
-        self.wv = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=lname+'/self/value')
-        self.dense = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=lname+'/output/dense')
-        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=lname+'/output/LayerNorm')
+        self.wq = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=bname+lname[0])
+        self.wk = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=bname+lname[1])
+        self.wv = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=bname+lname[2])
+        self.dense = keras.layers.Dense(self.head*self.size, None, True, w_initializing(), name=bname+lname[3])
+        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=bname+lname[4])
         self.drop = keras.layers.Dropout(drop)
 
     def transposing(self, x):
@@ -74,15 +75,14 @@ class Attention(keras.layers.Layer):
 
 class Transformer(keras.layers.Layer):
     """The transformer layer"""
-    def __init__(self, lname, head, size, dff, drop=0.1, **kwargs):
+    def __init__(self, bname, lname, head, size, dff, attdrop=0.1, drop=0.1, act='relu', **kwargs):
         super(Transformer, self).__init__(**kwargs)
         self.dim, self.dff = int(head*size), dff
-        self.att = Attention(lname+'/attention', head, size, drop)
-        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=lname+'/output/LayerNorm')
+        self.att = Attention(bname, lname, head, size, attdrop)
+        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=bname+lname[7])
         self.drop = keras.layers.Dropout(drop)
-        self.dense1 = keras.layers.Dense(self.dim, None, True, w_initializing(), name=lname+'/output/dense')
-        self.dense2 = keras.layers.Dense(
-            self.dff, gelu_activating, True, w_initializing(), name=lname+'/intermediate/dense')
+        self.dense1 = keras.layers.Dense(self.dim, None, True, w_initializing(), name=bname+lname[6])
+        self.dense2 = keras.layers.Dense(self.dff, act, True, w_initializing(), name=bname+lname[5])
 
     def propagating(self, x, training, mask):
         x1, a1 = self.att.propagating(x, training, mask)
@@ -92,13 +92,13 @@ class Transformer(keras.layers.Layer):
 
 class Embedding(keras.layers.Layer):
     """The embedding layer"""
-    def __init__(self, lname, voc, seg, dim, maxlen, drop=0.1, **kwargs):
+    def __init__(self, bname, lname, voc, seg, dim, maxlen, drop=0.1, **kwargs):
         super(Embedding, self).__init__(**kwargs)
         self.dim, self.maxlen = dim, maxlen
-        self.embedding = keras.layers.Embedding(voc, dim, w_initializing(), name=lname+'/word_embeddings')
-        self.segemb = keras.layers.Embedding(seg, dim, w_initializing(), name=lname+'/token_type_embeddings')
-        self.posemb = self.add_weight(lname+'/position_embeddings', (self.maxlen, self.dim), None, w_initializing())
-        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=lname+'/LayerNorm')
+        self.embedding = keras.layers.Embedding(voc, dim, w_initializing(), name=bname+lname[0])
+        self.segemb = keras.layers.Embedding(seg, dim, w_initializing(), name=bname+lname[1])
+        self.posemb = self.add_weight(bname+lname[2], (self.maxlen, self.dim), None, w_initializing())
+        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=bname+lname[3])
         self.drop = keras.layers.Dropout(drop)
 
     def posencoding(self, pos):
@@ -121,25 +121,71 @@ class Embedding(keras.layers.Layer):
 
 class BERT(keras.layers.Layer):
     """The BERT model"""
-    def __init__(self, config, **kwargs):
+    def __init__(self, config, albert=False, **kwargs):
         super(BERT, self).__init__(**kwargs)
+        self.albert = albert
         self.param = json.load(open(config)) if type(config) is str else config
-        self.embeddings = Embedding(
-            'bert/embeddings',
-            self.param['vocab_size'],
-            self.param['type_vocab_size'],
-            self.param['hidden_size'],
-            self.param['max_position_embeddings'],
-            self.param['hidden_dropout_prob'])
-        self.encoders = [Transformer(
-            'bert/encoder/layer_'+str(i1),
-            self.param['num_attention_heads'],
-            int(self.param['hidden_size']/self.param['num_attention_heads']),
-            self.param['intermediate_size'],
-            self.param['hidden_dropout_prob']) for i1 in range(self.param['num_hidden_layers'])]
+        self.act = gelu_activating if self.param['hidden_act'] == 'gelu' else self.param['hidden_act']
         self.replacement = {
             'bert/embeddings/word_embeddings/embeddings': 'bert/embeddings/word_embeddings',
             'bert/embeddings/token_type_embeddings/embeddings': 'bert/embeddings/token_type_embeddings'}
+        self.namea = [
+            '/attention_1/self/query',
+            '/attention_1/self/key',
+            '/attention_1/self/value',
+            '/attention_1/output/dense',
+            '/LayerNorm',
+            '/ffn_1/intermediate/dense',
+            '/ffn_1/intermediate/output/dense',
+            '/LayerNorm_1']
+        self.nameb = [
+            '/attention/self/query',
+            '/attention/self/key',
+            '/attention/self/value',
+            '/attention/output/dense',
+            '/attention/output/LayerNorm',
+            '/intermediate/dense',
+            '/output/dense',
+            '/output/LayerNorm']
+        self.namee = [
+            '/word_embeddings',
+            '/token_type_embeddings',
+            '/position_embeddings',
+            '/LayerNorm']
+        self.embedding = Embedding(
+            'bert/embeddings',
+            self.namee,
+            self.param['vocab_size'],
+            self.param['type_vocab_size'],
+            self.param['embedding_size'] if self.albert else self.param['hidden_size'],
+            self.param['max_position_embeddings'],
+            float(self.param['hidden_dropout_prob']))
+
+        if self.albert:
+            self.projection = keras.layers.Dense(
+                self.param['hidden_size'],
+                kernel_initializer=w_initializing(),
+                name='bert/encoder/embedding_hidden_mapping_in')
+            self.encoder = [Transformer(
+                'bert/encoder/transformer/group_0/inner_group_0',
+                self.namea,
+                self.param['num_attention_heads'],
+                int(self.param['hidden_size']/self.param['num_attention_heads']),
+                self.param['intermediate_size'],
+                float(self.param['attention_probs_dropout_prob']),
+                float(self.param['hidden_dropout_prob']),
+                self.act)]
+        else:
+            self.projection = None
+            self.encoder = [Transformer(
+                'bert/encoder/layer_'+str(i1),
+                self.nameb,
+                self.param['num_attention_heads'],
+                int(self.param['hidden_size']/self.param['num_attention_heads']),
+                self.param['intermediate_size'],
+                float(self.param['attention_probs_dropout_prob']),
+                float(self.param['hidden_dropout_prob']),
+                self.act) for i1 in range(self.param['num_hidden_layers'])]
 
     def loading(self, ckpt):
         _ = self.propagating(tf.ones((2, 2)), None, tf.zeros((2, 2)), False, False)
@@ -149,10 +195,16 @@ class BERT(keras.layers.Layer):
         keras.backend.batch_set_value(zip(tens1, valu1))
 
     def propagating(self, x, seg=None, mask=None, cls=False, training=False):
-        x1 = self.embeddings.propagating(x, seg, training)
+        x1 = self.embedding.propagating(x, seg, training)
+        x1 = self.projection(x1) if self.albert else x1
 
-        for i1 in range(self.param['num_hidden_layers']):
-            x1 = self.encoders[i1].propagating(x1, training, mask)
+        if self.albert:
+            for i1 in range(self.param['num_hidden_layers']):
+                x1 = self.encoder[0].propagating(x1, training, mask)
+
+        else:
+            for i1 in range(self.param['num_hidden_layers']):
+                x1 = self.encoder[i1].propagating(x1, training, mask)
 
         return tf.reshape(x1[:, 0, :], [-1, self.param['hidden_size']]) if cls else x1
 
@@ -160,11 +212,11 @@ class BERT(keras.layers.Layer):
 """Training optimizers"""
 
 
-class DecayingADAM(keras.optimizers.Optimizer):
-    """The ADAM optimizer with weight decay"""
-    def __init__(self, step, lrate=1e-3, drate=1e-2, b1=0.9, b2=0.999, epsilon=1e-7, name='DecayingAdam', **kwargs):
-        super(DecayingADAM, self).__init__(name, **kwargs)
-        self.drate, self.epsilon, self.step = drate, epsilon or keras.backend_config.epislon(), step
+class AdamW(keras.optimizers.Optimizer):
+    """The AdamW optimizer"""
+    def __init__(self, step, lrate=1e-3, drate=1e-2, b1=0.9, b2=0.999, eps=1e-6, lnorm=False, name='AdamW', **kwargs):
+        super(AdamW, self).__init__(name, **kwargs)
+        self.lnorm, self.step, self.drate, self.epsilon = lnorm, step, drate, eps or keras.backend_config.epislon()
         self._set_hyper('learning_rate', lrate)
         self._set_hyper('beta_1', b1)
         self._set_hyper('beta_2', b2)
@@ -178,10 +230,10 @@ class DecayingADAM(keras.optimizers.Optimizer):
         r1 = self._get_hyper('learning_rate', var_dtype)
         s1 = tf.cast(self.iterations, var_dtype)
         warm1 = tf.cast(int(self.step*0.1), var_dtype)
-        return keras.backend.switch(s1 < warm1, r1*s1/warm1, r1*(self.step-s1)/(self.step-warm1))
+        return tf.where(s1 < warm1, r1*s1/warm1, r1*(self.step-s1)/(self.step-warm1))
 
     def _resource_apply_base(self, grad, var, indices=None):
-        t1 = var.dtype.base_dtype
+        t1, name1 = var.dtype.base_dtype, var.name
         r1 = self._decayed_lr(t1)
         e1 = tf.convert_to_tensor(self.epsilon, t1)
         m1, v1 = self.get_slot(var, 'm'), self.get_slot(var, 'v')
@@ -201,39 +253,44 @@ class DecayingADAM(keras.optimizers.Optimizer):
                 m2 = self._resource_scatter_add(m1, indices, (1.0-beta1)*grad)
                 v2 = self._resource_scatter_add(v1, indices, (1.0-beta2)*grad*grad)
 
-        if 'LayerNorm' not in var.name and 'bias' not in var.name:
-            return tf.group(*[var.assign_sub(r1*(m2/(tf.sqrt(v2)+e1)+self.drate*var), self._use_locking), m2, v2])
-        else:
-            return tf.group(*[var.assign_sub(r1*(m2/(tf.sqrt(v2)+e1)), self._use_locking), m2, v2])
+        u1 = m2/(tf.sqrt(v2)+e1) if 'LayerNorm' in name1 or 'bias' in name1 else m2/(tf.sqrt(v2)+e1)+self.drate*var
 
-    def _resource_apply_dense(self, grad, var):
+        if self.lnorm and 'LayerNorm' not in name1 and 'bias' not in name1:
+            n1, n2 = tf.norm(var, 2), tf.norm(u1, 2)
+            rati1 = tf.where(tf.greater(n1, 0.), tf.where(tf.greater(n2, 0.), n1/n2, 1.), 1.)
+            return tf.group(*[var.assign_sub(rati1*r1*u1, self._use_locking), m2, v2])
+        else:
+            return tf.group(*[var.assign_sub(r1*u1, self._use_locking), m2, v2])
+
+    def _resource_apply_dense(self, grad, var, **kwargs):
         return self._resource_apply_base(grad, var)
 
-    def _resource_apply_sparse(self, grad, var, indices):
+    def _resource_apply_sparse(self, grad, var, indices, **kwargs):
         return self._resource_apply_base(grad, var, indices)
 
     def get_config(self):
-        conf1 = super(DecayingADAM, self).get_config()
+        conf1 = super(AdamW, self).get_config()
         conf1.update({
             'learning_rate': self._serialize_hyperparameter('learning_rate'),
             'beta_1': self._serialize_hyperparameter('beta_1'),
             'beta_2': self._serialize_hyperparameter('beta_2'),
+            'layer_norm': self.lnorm,
+            'step': self.step,
             'decaying_rate': self.drate,
-            'epsilon': self.epsilon,
-            'step': self.step})
+            'epsilon': self.epsilon})
         return conf1
 
 
-class LR(tf.keras.optimizers.schedules.LearningRateSchedule):
-    """The ADAM optimizer learning rate schedule"""
-    def __init__(self, dim, warmup=4000):
-        super(LR, self).__init__()
-        self.dim, self.warmup = tf.cast(dim, tf.float32), warmup
-
-    def __call__(self, step):
-        a1 = tf.math.rsqrt(step)
-        a2 = step*(self.warmup**-1.5)
-        return tf.math.rsqrt(self.dim)*tf.math.minimum(a1, a2)
+# class LRS(keras.optimizers.schedules.LearningRateSchedule):
+#     """The learning rate schedule of Adam optimizer"""
+#     def __init__(self, dim, warmup=4000):
+#         super(LRS, self).__init__()
+#         self.dim, self.warmup = tf.cast(dim, tf.float32), warmup
+#
+#     def __call__(self, step):
+#         a1 = tf.math.rsqrt(step)
+#         a2 = step*(self.warmup**-1.5)
+#         return tf.math.rsqrt(self.dim)*tf.math.minimum(a1, a2)
 
 
 """Model tools"""
@@ -270,7 +327,7 @@ class Tokenizer:
         b1 = self.separating(b)+['[SEP]'] if b is not None else []
         segm1 = ([0]*len(a1)+[1]*len(b1))[:maxlen]
         a1 = (a1+b1)[:maxlen]
-        segm1 = segm1+[segm1[-1]]*(maxlen-len(segm1))  # s1 = None if b is None else s1
+        segm1 = segm1+[segm1[-1]]*(maxlen-len(segm1))
         mask1 = [0]*len(a1)+[1]*(maxlen-len(a1))
         a1 = a1+['[PAD]']*(maxlen-len(a1))
         return [self.vocab.get(i1, self.vocab['[UNK]']) for i1 in a1], segm1, mask1
@@ -280,28 +337,13 @@ class Tokenizer:
 
 
 if __name__ == '__main__':
-    conf_1 = {
-        'attention_probs_dropout_prob': 0.1,
-        'directionality': 'bidi',
-        'hidden_act': 'gelu',
-        'hidden_dropout_prob': 0.1,
-        'hidden_size': 768,
-        'initializer_range': 0.02,
-        'intermediate_size': 3072,
-        'max_position_embeddings': 512,
-        'num_attention_heads': 12,
-        'num_hidden_layers': 12,
-        'pooler_fc_size': 768,
-        'pooler_num_attention_heads': 12,
-        'pooler_num_fc_layers': 3,
-        'pooler_size_per_head': 128,
-        'pooler_type': 'first_token_transform',
-        'type_vocab_size': 2,
-        'vocab_size': 21128}
+    # toke_1 = Tokenizer()
+    # toke_1.loading('./models/bert_base_ch/vocab.txt')
+    # bert_1 = BERT('./models/bert_base_ch/bert_config.json', False)
+    # bert_1.loading('./models/bert_base_ch/bert_model.ckpt')
     toke_1 = Tokenizer()
-    toke_1.loading('./bert/models/bert_base_ch/vocab.txt')
-    bert_1 = BERT(conf_1)
-    bert_1.loading('./bert/models/bert_base_ch/bert_model.ckpt')
+    toke_1.loading('./models/albert_base_ch/vocab_chinese.txt')
+    bert_1 = BERT('./models/albert_base_ch/albert_config.json', True)
+    bert_1.loading('./models/albert_base_ch/model.ckpt-best')
     text_1, segm_1, mask_1 = toke_1.encoding('我是头猪。', None, 128)
-    leng_1 = len('我是头猪。')+2
-    sequ_1 = bert_1.propagating(tf.constant([text_1]), tf.constant([segm_1]), tf.constant([mask_1]))[:, :leng_1, :]
+    sequ_1 = bert_1.propagating(tf.constant([text_1]), tf.constant([segm_1]), tf.constant([mask_1]))[:, :7, :]
