13130b57be5a03dbdaa05a75ab04b8d42d282e4e
RookieZB <chenzb_best@126.com>
2020-05-08 00:42:48 +0800

    Have added MobileNet.

--- mymodels.py
+++ mymodels.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Title: BERT by TF2
-Version: 0.1.1
+Title: My Implementation of Models by TensorFlow
 Author: Chris Chen
 Date: 2020/02/02
-Update: 2020/02/28
+Update: 2020/05/02
 
 """
 
@@ -30,11 +29,36 @@ def w_initializing(param=0.2):
 
 def gelu_activating(x):
     """The GELU activation function"""
-    g1 = 0.5*(1.0+tf.tanh((np.sqrt(2/np.pi)*(x+0.044715*tf.pow(x, 3)))))
-    return x*g1
+    return 0.5*x*(1.0+tf.tanh((np.sqrt(2/np.pi)*(x+0.044715*tf.pow(x, 3)))))
+
+
+def hswish_activating(x):
+    """The hard swish activation function"""
+    return x*tf.nn.relu6(x+np.float32(3))*np.float32(1./6.)
+
+
+def hsigmoid_activating(x):
+    """The hard sigmoid activation function"""
+    return tf.nn.relu6(x+3.)*0.16667
 
 
 """Network layers"""
+
+
+class InstanceNormalization(keras.layers.Layer):
+    """The instance normalization layer"""
+    def __init__(self, eps=1e-5):
+        super(InstanceNormalization, self).__init__()
+        self.epsilon, self.scale, self.offset = eps, None, None
+
+    def build(self, size):
+        self.scale = self.add_weight('scale', size[-1:], None, tf.random_normal_initializer(1., 0.02), trainable=True)
+        self.offset = self.add_weight('offset', size[-1:], None, 'zeros', trainable=True)
+
+    def propagating(self, x):
+        m1, v1 = tf.nn.moments(x, [1, 2], None, True)
+        norm1 = (x-m1)*tf.math.rsqrt(v1+self.epsilon)
+        return self.scale*norm1+self.offset
 
 
 class Attention(keras.layers.Layer):
@@ -66,7 +90,9 @@ class Attention(keras.layers.Layer):
         return tf.matmul(atte1, v), atte1
 
     def propagating(self, x, training, mask):
-        q1, k1, v1 = self.transposing(self.wq(x)), self.transposing(self.wk(x)), self.transposing(self.wv(x))
+        q1 = self.transposing(self.wq(x))
+        k1 = self.transposing(self.wk(x))
+        v1 = self.transposing(self.wv(x))
         x1, a1 = self.calculating(q1, k1, v1, mask)
         x1 = tf.transpose(x1, [0, 2, 1, 3])
         x1 = tf.reshape(x1, [-1, x1.shape[1], self.head*self.size])
@@ -101,22 +127,49 @@ class Embedding(keras.layers.Layer):
         self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=bname+lname[3])
         self.drop = keras.layers.Dropout(drop)
 
-    def posencoding(self, pos):
-        angl0 = 1/np.power(10000, (2*(np.arange(self.dim)[np.newaxis, :]//2))/np.float32(self.dim))
-        angl1 = np.arange(pos)[:, np.newaxis]*angl0
-        angl1[:, 0::2] = np.sin(angl1[:, 0::2])
-        angl1[:, 1::2] = np.cos(angl1[:, 1::2])
-        return tf.cast(angl1[np.newaxis, ...], dtype=tf.float32)
-
     def propagating(self, x, seg, training):
-        x1 = self.embedding(x)  # x1 = x1*tf.math.sqrt(tf.cast(self.dim, tf.float32))
+        x1 = self.embedding(x)
         l1 = tf.shape(x)
         x2 = self.segemb(tf.zeros((l1[0], l1[1]))) if seg is None else self.segemb(seg)
         p1 = tf.slice(self.posemb, [0, 0], [l1[1], -1])
         return self.drop(self.norm(x1+x2+p1), training=training)
 
 
-"""BERT models"""
+class Bottleneck(keras.layers.Layer):
+    """The bottleneck layer"""
+    def __init__(self, lname, size, stride, cin, cout, expansion, caxis, act, squeeze=True, **kwargs):
+        super(Bottleneck, self).__init__(**kwargs)
+        self.div, self.cin, self.exp, self.filter = 8, cin, int(cin*expansion), cout
+        self.stride, self.caxis, self.squeeze = stride, caxis, squeeze
+        self.factor = self.dividing(self.exp/4)
+        self.act = hswish_activating if act == 'hswish' else tf.nn.relu
+        self.conv1 = keras.layers.Conv2D(self.exp, 1, 1, 'same', use_bias=False, name=lname+'expand/')
+        self.conv2 = keras.layers.DepthwiseConv2D(size, stride, 'same', use_bias=False, name=lname+'depthwise/')
+        self.conv3 = keras.layers.Conv2D(cout, 1, 1, 'same', use_bias=False, name=lname+'project/')
+        self.norm1 = keras.layers.BatchNormalization(caxis, 0.999, name=lname+'expand/BatchNorm/')
+        self.norm2 = keras.layers.BatchNormalization(caxis, 0.999, name=lname+'depthwise/BatchNorm/')
+        self.norm3 = keras.layers.BatchNormalization(caxis, 0.999, name=lname+'project/BatchNorm/')
+        self.pool = keras.layers.GlobalAveragePooling2D()
+        self.s1 = keras.layers.Conv2D(self.factor, 1, activation='relu', name=lname+'squeeze_excite/Conv/')
+        self.s2 = keras.layers.Conv2D(self.exp, 1, activation=hsigmoid_activating, name=lname+'squeeze_excite/Conv_1/')
+
+    def dividing(self, v, minv=None):
+        valu1 = max(self.div if minv is None else minv, int(v+self.div/2)//self.div*self.div)
+        return valu1+self.div if valu1 < 0.9*v else valu1
+
+    def propagating(self, x, training):
+        x1 = self.act(self.norm1(self.conv1(x), training=training)) if self.cin != self.exp else x
+        x1 = self.act(self.norm2(self.conv2(x1), training=training))
+
+        if self.squeeze:
+            h1 = tf.reshape(self.pool(x1), [-1, 1, 1, self.exp])
+            x1 = x1*self.s2(self.s1(h1))
+
+        x1 = self.norm3(self.conv3(x1), training=training)
+        return x+x1 if self.cin == self.filter and self.stride == 1 else x1
+
+
+"""Model implementations"""
 
 
 class BERT(keras.layers.Layer):
@@ -189,7 +242,8 @@ class BERT(keras.layers.Layer):
 
     def loading(self, ckpt):
         _ = self.propagating(tf.ones((2, 2)), None, tf.zeros((2, 2)), False, False)
-        tens1, name1 = self.weights, [i1.name[:-2] for i1 in self.weights]
+        tens1 = self.weights
+        name1 = [i1.name[:-2] for i1 in self.weights]
         name1 = [i1 if i1 not in self.replacement.keys() else self.replacement[i1] for i1 in name1]
         valu1 = [tf.train.load_variable(ckpt, i1) for i1 in name1]
         keras.backend.batch_set_value(zip(tens1, valu1))
@@ -209,14 +263,71 @@ class BERT(keras.layers.Layer):
         return tf.reshape(x1[:, 0, :], [-1, self.param['hidden_size']]) if cls else x1
 
 
+class MobileNet(keras.layers.Layer):
+    """The MobileNet V3 model"""
+    def __init__(self, alpha, caxis, cate, act='hswish', **kwargs):
+        super(MobileNet, self).__init__(**kwargs)
+        self.div, self.encoder = 8, []
+        self.act = hswish_activating if act == 'hswish' else act
+        self.conv1 = keras.layers.Conv2D(16, 3, 2, 'same', use_bias=False, name='MobilenetV3/Conv/')
+        self.conv2 = keras.layers.Conv2D(576, 1, use_bias=False, name='MobilenetV3/Conv_1/')
+        self.conv3 = keras.layers.Conv2D(1024, 1, use_bias=False, name='MobilenetV3/Conv_2/')
+        self.conv4 = keras.layers.Conv2D(cate, 1, use_bias=False, name='MobilenetV3/Logits/Conv2d_1c_1x1')
+        self.norm1 = keras.layers.BatchNormalization(caxis, 0.999, 1e-3, name='MobilenetV3/Conv/'+'BatchNorm/')
+        self.norm2 = keras.layers.BatchNormalization(caxis, 0.999, 1e-3, name='MobilenetV3/Conv_1/'+'BatchNorm/')
+        self.pool = keras.layers.GlobalAveragePooling2D()
+        self.param = [
+            (16*alpha,),
+            (16*alpha, 3, 2, 1, 'relu', True),
+            (24*alpha, 3, 2, 72/16, 'relu', False),
+            (24*alpha, 3, 1, 88/24, 'relu', False),
+            (40*alpha, 5, 2, 4, 'hswish', True),
+            (40*alpha, 5, 1, 6, 'hswish', True),
+            (40*alpha, 5, 1, 6, 'hswish', True),
+            (48*alpha, 5, 1, 3, 'hswish', True),
+            (48*alpha, 5, 1, 3, 'hswish', True),
+            (96*alpha, 5, 2, 6, 'hswish', True),
+            (96*alpha, 5, 1, 6, 'hswish', True),
+            (96*alpha, 5, 1, 6, 'hswish', True)]
+
+        for i1 in range(1, len(self.param)):
+            p1, p2, p3, p4, p5, p6 = self.param[i1]
+            name1 = 'MobilenetV3/expanded_conv_'+str(i1-1)+'/' if i1 > 1 else 'MobilenetV3/expanded_conv/'
+            self.encoder += [Bottleneck(name1, p2, p3, self.param[i1-1][0], p1, p4, caxis, p5, p6)]
+
+    def dividing(self, v, minv=None):
+        valu1 = max(self.div if minv is None else minv, int(v+self.div/2)//self.div*self.div)
+        return valu1+self.div if valu1 < 0.9*v else valu1
+
+    def loading(self, ckpt):
+        _ = self.propagating(tf.ones((2, 224, 224, 3)))
+        tens1 = self.weights
+        name1 = [i1.name[:-2] for i1 in self.weights]
+        name1 = [i1.replace('kernel', 'weights').replace('bias', 'biases') for i1 in name1]
+        valu1 = [tf.train.load_variable(ckpt, i1) for i1 in name1]
+        keras.backend.batch_set_value(zip(tens1, valu1))
+
+    def propagating(self, x, training=False):
+        x1 = self.act(self.norm1(self.conv1(x), training=training))
+
+        for i1 in self.encoder:
+            x1 = i1.propagating(x1, training)
+
+        x1 = self.act(self.norm2(self.conv2(x1), training=training))
+        x1 = tf.reshape(self.pool(x1), (-1, 1, 1, x1.shape[-1]))
+        x1 = self.act(self.conv3(x1))
+        return tf.nn.softmax(self.conv4(x1))
+
+
 """Training optimizers"""
 
 
 class AdamW(keras.optimizers.Optimizer):
-    """The AdamW optimizer"""
+    """The AdamW optimizer and LAMB optimizer"""
     def __init__(self, step, lrate=1e-3, drate=1e-2, b1=0.9, b2=0.999, eps=1e-6, lnorm=False, name='AdamW', **kwargs):
         super(AdamW, self).__init__(name, **kwargs)
-        self.lnorm, self.step, self.drate, self.epsilon = lnorm, step, drate, eps or keras.backend_config.epislon()
+        self.lnorm, self.step, self.drate = lnorm, step, drate
+        self.epsilon = eps or keras.backend_config.epislon()
         self._set_hyper('learning_rate', lrate)
         self._set_hyper('beta_1', b1)
         self._set_hyper('beta_2', b2)
@@ -230,17 +341,17 @@ class AdamW(keras.optimizers.Optimizer):
         r1 = self._get_hyper('learning_rate', var_dtype)
         s1 = tf.cast(self.iterations, var_dtype)
         warm1 = tf.cast(int(self.step*0.1), var_dtype)
-        return tf.where(s1 < warm1, r1*s1/warm1, r1*(self.step-s1)/(self.step-warm1))
+        return tf.where(s1 < warm1, r1*s1/warm1, r1*(self.step-s1)/(self.step-warm1)) if not self.lnorm else r1
 
     def _resource_apply_base(self, grad, var, indices=None):
-        t1, name1 = var.dtype.base_dtype, var.name
+        t1 = var.dtype.base_dtype
+        name1 = var.name
         r1 = self._decayed_lr(t1)
         e1 = tf.convert_to_tensor(self.epsilon, t1)
-        m1, v1 = self.get_slot(var, 'm'), self.get_slot(var, 'v')
-        beta1, beta2 = tf.identity(self._get_hyper('beta_1', t1)), tf.identity(self._get_hyper('beta_2', t1))
-        # step1 = tf.cast(self.iterations+1, t1)
-        # powe1, powe2 = tf.pow(beta1, step1), tf.pow(beta2, step1)
-        # rate1 = rate1*tf.sqrt(1-powe2)/(1-powe1)
+        m1 = self.get_slot(var, 'm')
+        v1 = self.get_slot(var, 'v')
+        beta1 = tf.identity(self._get_hyper('beta_1', t1))
+        beta2 = tf.identity(self._get_hyper('beta_2', t1))
 
         if indices is None:
             m2 = m1.assign(beta1*m1+(1.0-beta1)*grad, self._use_locking)
@@ -256,7 +367,8 @@ class AdamW(keras.optimizers.Optimizer):
         u1 = m2/(tf.sqrt(v2)+e1) if 'LayerNorm' in name1 or 'bias' in name1 else m2/(tf.sqrt(v2)+e1)+self.drate*var
 
         if self.lnorm and 'LayerNorm' not in name1 and 'bias' not in name1:
-            n1, n2 = tf.norm(var, 2), tf.norm(u1, 2)
+            n1 = tf.norm(var, 2)
+            n2 = tf.norm(u1, 2)
             rati1 = tf.where(tf.greater(n1, 0.), tf.where(tf.greater(n2, 0.), n1/n2, 1.), 1.)
             return tf.group(*[var.assign_sub(rati1*r1*u1, self._use_locking), m2, v2])
         else:
@@ -281,23 +393,27 @@ class AdamW(keras.optimizers.Optimizer):
         return conf1
 
 
-# class LRS(keras.optimizers.schedules.LearningRateSchedule):
-#     """The learning rate schedule of Adam optimizer"""
-#     def __init__(self, dim, warmup=4000):
-#         super(LRS, self).__init__()
-#         self.dim, self.warmup = tf.cast(dim, tf.float32), warmup
-#
-#     def __call__(self, step):
-#         a1 = tf.math.rsqrt(step)
-#         a2 = step*(self.warmup**-1.5)
-#         return tf.math.rsqrt(self.dim)*tf.math.minimum(a1, a2)
+class MovingAverage(object):
+    """The exponential moving average strategy"""
+    def __init__(self, var, decay=0.99):
+        self.decay = decay
+        self.var = [keras.backend.zeros(w.shape) for w in var]
+        keras.backend.batch_set_value(zip(self.var, keras.backend.batch_get_value(var)))
+        self.updating(var)
+
+    def updating(self, var):
+        for w1, w2 in zip(self.var, var):
+            keras.backend.moving_average_update(w1, w2, self.decay)
+
+    def setting(self, var):
+        keras.backend.batch_set_value(zip(var, keras.backend.batch_get_value(self.var)))
 
 
 """Model tools"""
 
 
 class Tokenizer:
-    """The text tokenizer"""
+    """The Chinese tokenizer"""
     def __init__(self):
         self.vocab = {}
         self.character = [
@@ -310,7 +426,8 @@ class Tokenizer:
                 self.vocab[j1.strip()] = i1
 
     def separating(self, text):
-        text1, char1 = [], False
+        text1 = []
+        char1 = False
 
         for c1 in text:
             for i1 in self.character:
@@ -318,7 +435,8 @@ class Tokenizer:
                     char1 = True
                     break
 
-            text1, char1 = text1+[' ', c1, ' '] if char1 else text1+[c1], False
+            text1 = text1+[' ', c1, ' '] if char1 else text1+[c1]
+            char1 = False
 
         return ''.join(text1).strip().split()
 
@@ -333,17 +451,26 @@ class Tokenizer:
         return [self.vocab.get(i1, self.vocab['[UNK]']) for i1 in a1], segm1, mask1
 
 
-"""BERT tests"""
+"""Model tests"""
 
 
-if __name__ == '__main__':
-    # toke_1 = Tokenizer()
-    # toke_1.loading('./models/bert_base_ch/vocab.txt')
-    # bert_1 = BERT('./models/bert_base_ch/bert_config.json', False)
-    # bert_1.loading('./models/bert_base_ch/bert_model.ckpt')
-    toke_1 = Tokenizer()
-    toke_1.loading('./models/albert_base_ch/vocab_chinese.txt')
-    bert_1 = BERT('./models/albert_base_ch/albert_config.json', True)
-    bert_1.loading('./models/albert_base_ch/model.ckpt-best')
-    text_1, segm_1, mask_1 = toke_1.encoding('我是头猪。', None, 128)
-    sequ_1 = bert_1.propagating(tf.constant([text_1]), tf.constant([segm_1]), tf.constant([mask_1]))[:, :7, :]
+def bert_testing():
+    """The test of BERT"""
+    toke1 = Tokenizer()
+    toke1.loading('./models/albert_base_ch/vocab_chinese.txt')
+    bert1 = BERT('./models/albert_base_ch/albert_config.json', True)
+    bert1.loading('./models/albert_base_ch/model.ckpt-best')
+    text1, segm1, mask1 = toke1.encoding('我是头猪。', None, 128)
+    print(bert1.propagating(tf.constant([text1]), tf.constant([segm1]), tf.constant([mask1]))[:, :7, :])
+
+
+def mobile_testing():
+    """The test of MobileNet"""
+    imag1 = tf.image.decode_jpeg(tf.io.read_file('./models/v3_small_float/panda.jpg'), channels=3)
+    imag1 = tf.expand_dims(tf.image.resize(tf.cast(imag1, tf.float32)/128.-1., [224, 224]), 0)
+    mnet1 = MobileNet(1, -1, 1001)
+    mnet1.loading('./models/v3_small_float/model-388500')
+    inpu1 = tf.keras.Input(shape=(224, 224, 3))
+    outp1 = mnet1.propagating(inpu1)
+    mode1 = tf.keras.Model(inputs=inpu1, outputs=outp1)
+    print(np.flipud(np.argsort(mode1.predict(imag1)[0, 0, 0, :]))[:5])
