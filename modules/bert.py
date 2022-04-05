b9159eb04ebca7d3cae6eb467e76a7fbb4d282a3
RookieZB <chenzb_best@126.com>
2022-04-05 17:55:12 +0800

    Nothing special.

--- modules/bert.py
+++ modules/bert.py
@@ -48,8 +48,12 @@ class Attention(keras.layers.Layer):
         a1 = tf.nn.softmax(a1+self.masking(mask) if mask is not None else a1, axis=-1)
         return tf.matmul(self.attdrop(a1, training=training), self.transposing(v)), a1
 
-    def propagating(self, x, mask=None, training=False):
-        x1, a1 = self.calculating(self.wq(x), self.wk(x), self.wv(x), mask, training)
+    def propagating(self, x, mask=None, training=False, past=None):
+        q1, k1, v1 = self.wq(x), self.wk(x), self.wv(x)
+        k1 = k1 if past is None else tf.concat([past[0], k1], axis=-2)
+        v1 = v1 if past is None else tf.concat([past[1], v1], axis=-2)
+        m1 = mask if past is None else tf.concat([tf.zeros([tf.shape(x)[0], tf.shape(past[0])[1]], tf.int32), mask], 1)
+        x1, a1 = self.calculating(q1, k1, v1, m1, training)
         x1 = tf.reshape(tf.transpose(x1, [0, 2, 1, 3]), [-1, tf.shape(x1)[2], self.dim])
         return self.norm(x+self.drop(self.dense(x1), training=training)), a1
 
@@ -63,8 +67,8 @@ class TransEncoder(keras.layers.Layer):
         self.dense2 = keras.layers.Dense(int(head*size), None, True, w_initializing(), name=bname+lname[6])
         self.drop = keras.layers.Dropout(drop)
 
-    def propagating(self, x, mask=None, training=False):
-        x1, a1 = self.att.propagating(x, mask, training)
+    def propagating(self, x, mask=None, training=False, past=None):
+        x1, a1 = self.att.propagating(x, mask, training, past)
         return self.norm(x1+self.drop(self.dense2(self.dense1(x1)), training=training))
 
 
@@ -83,87 +87,73 @@ class Embedding(keras.layers.Layer):
 
 
 class BERT(keras.layers.Layer):
-    def __init__(self, config, model='bert', mode='seq', fromtf=True, **kwargs):
+    def __init__(self, config, model='bert', mlm=False, fromtf=True, **kwargs):
         super(BERT, self).__init__(**kwargs)
-        self.model, self.mode, self.cat, self.fromtf = model.lower(), mode.lower(), 0, fromtf
+        assert model in ['bert', 'roberta', 'albert', 'electra']
+        self.head, self.share = model if model in ['bert', 'electra'] else 'bert', (model == 'albert')
         self.param = json.load(open(config)) if type(config) is str else config
-        self.embsize = self.param.get('embedding_size', self.param['hidden_size'])
+        self.edim = self.param.get('embedding_size', self.param['hidden_size'])
         self.act = gelu_activating if self.param['hidden_act'] == 'gelu' else self.param['hidden_act']
-        self.torchrpl = {'/': '.', 'layer_': 'layer.', 'kernel': 'weight', '_embeddings': '_embeddings.weight'}
-        self.rpl = {'': ''} if self.fromtf else self.torchrpl
-
-        if self.model in ['bert', 'albert', 'electra', 'roberta'] or self.model.split('*')[0] in ['bert', 'albert']:
-            self.cat = 1 if self.model.split('*')[0] == 'albert' else 0
-            self.hd = 'bert' if self.model in ['albert', 'roberta'] else self.model.split('*')[-1]
-            self.namep = self.hd+('/encoder/embedding_hidden_mapping_in' if self.cat else '/embeddings_project')
-            self.namee = ['/word_embeddings', '/position_embeddings', '/token_type_embeddings', '/LayerNorm']
-            self.namel = ['cls/predictions/'+i1 for i1 in ['transform/dense', 'transform/LayerNorm', 'output_bias']]
-            self.namea = [
-                '/attention/self/query',
-                '/attention/self/key',
-                '/attention/self/value',
-                '/attention/output/dense',
-                '/attention/output/LayerNorm',
-                '/intermediate/dense',
-                '/output/dense',
-                '/output/LayerNorm']
-            self.nameb = [
-                '/attention_1/self/query',
-                '/attention_1/self/key',
-                '/attention_1/self/value',
-                '/attention_1/output/dense',
-                '/LayerNorm',
-                '/ffn_1/intermediate/dense',
-                '/ffn_1/intermediate/output/dense',
-                '/LayerNorm_1']
-            self.embedding = Embedding(
-                self.hd+'/embeddings',
-                self.namee,
-                self.param['vocab_size'],
-                self.embsize,
-                self.param['max_position_embeddings'],
-                self.param['type_vocab_size'],
-                float(self.param['hidden_dropout_prob']))
-            self.projection = keras.layers.Dense(
-                self.param['hidden_size'],
-                kernel_initializer=w_initializing(),
-                name=self.namep) if self.embsize != self.param['hidden_size'] else None
-            self.encoder = [TransEncoder(
-                self.hd+('/encoder/transformer/group_0/inner_group_0' if self.cat else '/encoder/layer_'+str(i1)),
-                self.nameb if self.cat else self.namea,
-                self.param['num_attention_heads'],
-                self.param['hidden_size']//self.param['num_attention_heads'],
-                self.param['intermediate_size'],
-                float(self.param['attention_probs_dropout_prob']),
-                float(self.param['hidden_dropout_prob']),
-                self.act) for i1 in range(1 if self.cat else self.param['num_hidden_layers'])]
-
-            if self.mode == 'mlm':
-                self.dense = keras.layers.Dense(self.embsize, self.act, True, w_initializing(), name=self.namel[0])
-                self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=self.namel[1])
-                self.outbias = self.add_weight(self.namel[2], self.param['vocab_size'], None, 'zeros')
-
-        else:
-            raise Exception('Unrecognized model "{}".'.format(self.model))
+        self.nc = ['cls/predictions/'+i1 for i1 in ['transform/dense', 'transform/LayerNorm', 'output_bias']]
+        self.na = [
+            '/attention/self/query',
+            '/attention/self/key',
+            '/attention/self/value',
+            '/attention/output/dense',
+            '/attention/output/LayerNorm',
+            '/intermediate/dense',
+            '/output/dense',
+            '/output/LayerNorm']
+        self.nb = [
+            '/attention_1/self/query',
+            '/attention_1/self/key',
+            '/attention_1/self/value',
+            '/attention_1/output/dense',
+            '/LayerNorm',
+            '/ffn_1/intermediate/dense',
+            '/ffn_1/intermediate/output/dense',
+            '/LayerNorm_1']
+        self.embedding = Embedding(
+            self.head+'/embeddings',
+            ['/word_embeddings', '/position_embeddings', '/token_type_embeddings', '/LayerNorm'],
+            self.param['vocab_size'],
+            self.edim,
+            self.param['max_position_embeddings'],
+            self.param['type_vocab_size'],
+            float(self.param['hidden_dropout_prob']))
+        self.projection = keras.layers.Dense(
+            self.param['hidden_size'],
+            name=self.head+('/encoder/embedding_hidden_mapping_in' if self.share else '/embeddings_project'),
+            kernel_initializer=w_initializing()) if self.edim != self.param['hidden_size'] else None
+        self.encoder = [TransEncoder(
+            self.head+('/encoder/transformer/group_0/inner_group_0' if self.share else '/encoder/layer_'+str(i1)),
+            self.nb if self.share else self.na,
+            self.param['num_attention_heads'],
+            self.param['hidden_size']//self.param['num_attention_heads'],
+            self.param['intermediate_size'],
+            float(self.param['attention_probs_dropout_prob']),
+            float(self.param['hidden_dropout_prob']),
+            self.act) for i1 in range(1 if self.share else self.param['num_hidden_layers'])]
+        self.dense = keras.layers.Dense(self.edim, self.act, True, w_initializing(), name=self.nc[0]) if mlm else None
+        self.norm = keras.layers.LayerNormalization(-1, 1e-6, name=self.nc[1]) if mlm else None
+        self.outbias = self.add_weight(self.nc[2], self.param['vocab_size'], None, 'zeros') if mlm else None
+        self.fromtf, self.rpl = fromtf, {'': ''} if fromtf else {
+            '/': '.', 'layer_': 'layer.', 'kernel': 'weight', '_embeddings': '_embeddings.weight', 'output_': ''}
 
     def loading(self, ckpt):
         _ = self.propagating(tf.ones((2, 2), tf.int32), tf.zeros((2, 2), tf.int32), tf.zeros((2, 2)))
-        r1 = re.compile('|'.join(map(re.escape, self.rpl)))
+        r1, l1 = re.compile('|'.join(map(re.escape, self.rpl))), torch.load(ckpt) if not self.fromtf else None
         n1 = [r1.sub((lambda x1: self.rpl[x1.group(0)]), i1.name[:-2]) for i1 in self.weights]
-        l1 = torch.load(ckpt) if not self.fromtf else None
         keras.backend.batch_set_value(zip(self.weights, [tf.train.load_variable(ckpt, i1) if self.fromtf else l1[
             i1].numpy().T if 'weight' in i1 and 'embeddings.weight' not in i1 else l1[i1].numpy() for i1 in n1]))
 
-    def propagating(self, x, seg, mask, training=False, softmax=True):
+    def propagating(self, x, seg, mask, head=False, training=False, past=None):
         x1, x2 = self.embedding.propagating(x, seg, training=training), None
         x1 = self.projection(x1) if self.projection else x1
 
         for i1 in range(self.param['num_hidden_layers']):
-            x1 = self.encoder[0 if self.cat == 1 else i1].propagating(x1, mask, training=training)
+            x1 = self.encoder[0 if self.share else i1].propagating(x1, mask, training, past[i1] if past else None)
 
-        if self.mode == 'mlm':
-            x2 = self.norm(self.dense(x1))
-            x2 = tf.nn.bias_add(tf.matmul(x2, self.embedding.emb, transpose_b=True), self.outbias)
-            x2 = tf.nn.softmax(x2) if softmax else x2
-
-        return (x2, x1) if self.mode == 'mlm' else x1 if self.mode == 'seq' else x1[:, 0, :]
+        x2 = self.norm(self.dense(x1)) if head else None
+        x2 = tf.nn.bias_add(tf.matmul(x2, self.embedding.emb, transpose_b=True), self.outbias) if head else None
+        return (x2, x1) if head else x1
